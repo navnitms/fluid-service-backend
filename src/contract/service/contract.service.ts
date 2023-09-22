2cb71287fef2b30ee89e3264cf9ee79c26189039
navnitms <ms.navnit@gmail.com>
2023-09-22 21:54:10 +0530

    Add shortId to contract

--- src/contract/service/contract.service.ts
+++ src/contract/service/contract.service.ts
@@ -1,21 +1,33 @@
-import { BadRequestException, Injectable, Logger } from '@nestjs/common';
+import {
+  BadRequestException,
+  Inject,
+  Injectable,
+  Logger,
+} from '@nestjs/common';
 import { DataSource, DeepPartial, EntityManager } from 'typeorm';
 import { Contract } from '../entity/contract.entity';
 import { ContractStatus, CreateContractInput } from 'src/schema/graphql.schema';
 import { v4 } from 'uuid';
 import { ContractProduct } from '../entity/contract.product.entity';
+import { TenantSettingService } from 'src/tenant/service/tenant.setting.service';
 
 @Injectable()
 export class ContractService {
   private logger: Logger = new Logger(ContractService.name);
 
-  constructor(private readonly dataSource: DataSource) {}
+  constructor(
+    private readonly dataSource: DataSource,
+    @Inject(TenantSettingService)
+    private readonly tenantSettingService: TenantSettingService,
+  ) {}
 
   async createContract(contractInput: CreateContractInput): Promise<Contract> {
     const { contractProducts, ...contractInputDetails } = contractInput;
+    const shortId = await this.getShortContractId(contractInput.tenantId);
 
     const contract: DeepPartial<Contract> = {
       id: v4(),
+      shortId,
       ...contractInputDetails,
     };
     if (contractInputDetails.startDate > contractInputDetails.endDate) {
@@ -72,5 +84,20 @@ export class ContractService {
       .offset(offset)
       .limit(limit)
       .getMany();
+  }
+
+  private async getShortContractId(tenantId: string): Promise<string> {
+    const tenantSettings =
+      await this.tenantSettingService.getTenantSettingsByTenantId(tenantId);
+
+    const lastContract = await this.dataSource
+      .getRepository(Contract)
+      .findOne({ where: { tenantId }, order: { createdAt: 'DESC' } });
+
+    const nextNumber = lastContract
+      ? parseInt(lastContract.shortId.split('-')[1]) + 1
+      : 1;
+
+    return `${tenantSettings.shortCode}-${nextNumber}`;
   }
 }
